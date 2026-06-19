b8699b3bf3b2824bb35e8262e012f564542eec34
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-19 15:35:17 +0300

    calculation

--- hello.dart
+++ hello.dart
@@ -1,5 +1,17 @@
 void main() {
-  var firstName = "ishaq";
-  var lastName = "aliyu muhammad";
-  print("My FullName is $firstName $lastName");
+  int num1 = 10; //declaring number1
+  int num2 = 3; //declaring number2
+
+  // Calculation
+  int sum = num1 + num2;
+  int diff = num1 - num2;
+  int mul = num1 * num2;
+  double div =
+      num1 / num2; // It is double because it outputs number with decimal.
+
+  // displaying the output
+  print("The sum is $sum");
+  print("The diff is $diff");
+  print("The mul is $mul");
+  print("The div is $div");
 }
