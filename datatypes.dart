8528c28c1493ea5589b0b853817f3003362a22cc
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-19 17:36:48 +0300

    var

--- datatypes.dart
+++ datatypes.dart
@@ -1,9 +1,7 @@
 void main() {
-  Map<String, String> myDetails = {
-    'name': 'John Doe',
-    'address': 'USA',
-    'fathername': 'Soe Doe',
-  };
-  // displaying the output
-  print(myDetails['name']);
+  var name = "John Doe"; // String
+  var age = 20; // int
+
+  print(name);
+  print(age);
 }
