178458d8ae38bd5e0c66d89c36eced0847565524
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-19 22:11:48 +0300

    int userinput

--- userInput.dart
+++ userInput.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void main() {
-  print("Enter name:");
-  String? name = stdin.readLineSync();
-  print("The entered name is ${name}");
+  print("Enter number:");
+  int? number = int.parse(stdin.readLineSync()!);
+  print("The entered number is ${number}");
 }
