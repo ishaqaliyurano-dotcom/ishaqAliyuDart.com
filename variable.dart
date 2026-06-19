c251b7253568e22720ec1850db6bbeb4228a231f
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-19 15:45:25 +0300

    declaring variable

---
+++ variable.dart
@@ -0,0 +1,8 @@
+void main() {
+  // Declaring Values
+  String schoolName = "Diamond School";
+  String addresss = "New York 2140";
+
+  // Printing Values
+  print("School name is $schoolName and address is $addresss");
+}
