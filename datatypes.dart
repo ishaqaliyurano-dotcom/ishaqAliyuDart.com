5519a2d007cc72ed1fde2d7468b54d0827e53e7a
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-19 17:05:17 +0300

    round double value

--- datatypes.dart
+++ datatypes.dart
@@ -1,15 +1,5 @@
 void main() {
-  // declaring variables
-  String name = "John";
-  String address = "USA";
-  num age = 20; // used to store any types of numbers
-  num height = 5.9;
-  bool isMarried = false;
-
-  // printing variables value
-  print("Name is $name");
-  print("Address is $address");
-  print("Age is $age");
-  print("Height is $height");
-  print("Married Status is $isMarried");
+  // Declaring Variables
+  double price = 1130.2232323233233; // valid.
+  print(price.toStringAsFixed(2));
 }
