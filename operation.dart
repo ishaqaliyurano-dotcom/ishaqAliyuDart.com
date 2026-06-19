ad3b2e777507752007376f6940aef1d3a4e30a5f
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-19 21:46:09 +0300

    assignment operation

--- operation.dart
+++ operation.dart
@@ -1,23 +1,11 @@
 void main() {
-  // declaring two numbers
-  int num1 = 10;
-  int num2 = 3;
-
-  // performing arithmetic calculation
-  int sum = num1 + num2; // addition
-  int diff = num1 - num2; // subtraction
-  int unaryMinus = -num1; // unary minus
-  int mul = num1 * num2; // multiplication
-  double div = num1 / num2; // division
-  int div2 = num1 ~/ num2; // integer division
-  int mod = num1 % num2; // show remainder
-
-  //Printing info
-  print("The addition is $sum.");
-  print("The subtraction is $diff.");
-  print("The unary minus is $unaryMinus.");
-  print("The multiplication is $mul.");
-  print("The division is $div.");
-  print("The integer division is $div2.");
-  print("The modulus is $mod.");
+  double age = 24;
+  age += 1; // Here age+=1 means age = age + 1.
+  print("After Addition Age is $age");
+  age -= 1; //Here age-=1 means age = age - 1.
+  print("After Subtraction Age is $age");
+  age *= 2; //Here age*=2 means age = age * 2.
+  print("After Multiplication Age is $age");
+  age /= 2; //Here age/=2 means age = age / 2.
+  print("After Division Age is $age");
 }
