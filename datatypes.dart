fde501f95ccbd3e9bc0f98110600c6e1d46db6d5
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-19 17:18:58 +0300

    str to double

--- datatypes.dart
+++ datatypes.dart
@@ -1,8 +1,8 @@
 void main() {
-  String strvalue = "1";
+  String strvalue = "1.1";
   print("Type of strvalue is ${strvalue.runtimeType}");
-  int intvalue = int.parse(strvalue);
-  print("Value of intvalue is $intvalue");
+  double doublevalue = double.parse(strvalue);
+  print("Value of doublevalue is $doublevalue");
   // this will print data type
-  print("Type of intvalue is ${intvalue.runtimeType}");
+  print("Type of doublevalue is ${doublevalue.runtimeType}");
 }
