b8bf8c67d9bb870982d53d37ce01285aa4c4b5bc
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-19 21:58:28 +0300

    type test

--- operation.dart
+++ operation.dart
@@ -1,11 +1,7 @@
 void main() {
-  int userid = 123;
-  int userpin = 456;
+  String value1 = "Dart Tutorial";
+  int age = 10;
 
-  // Printing Info
-  print((userid == 123) && (userpin == 456)); // print true
-  print((userid == 1213) && (userpin == 456)); // print false.
-  print((userid == 123) || (userpin == 456)); // print true.
-  print((userid == 1213) || (userpin == 456)); // print true
-  print((userid == 123) != (userpin == 456)); //print false
+  print(value1 is String);
+  print(age is! int);
 }
