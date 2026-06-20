131719b8ad1200f8d6443e56704d26d9b8600805
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 18:42:00 +0300

    private properties

--- OOP.dart
+++ OOP.dart
@@ -1,21 +1,10 @@
 class Employee {
-  // Private properties
-  int? _id;
-  String? _name;
-
-  // Getter method to access private property _id
-  int getId() {
-    return _id!;
-  }
+  // Private property
+  var _name;
 
   // Getter method to access private property _name
   String getName() {
-    return _name!;
-  }
-
-  // Setter method to update private property _id
-  void setId(int id) {
-    this._id = id;
+    return _name;
   }
 
   // Setter method to update private property _name
@@ -25,13 +14,7 @@ class Employee {
 }
 
 void main() {
-  // Create an object of Employee class
-  Employee emp = new Employee();
-  // setting values to the object using setter
-  emp.setId(1);
-  emp.setName("John");
-
-  // Retrieve the values of the object using getter
-  print("Id: ${emp.getId()}");
-  print("Name: ${emp.getName()}");
+  var employee = Employee();
+  employee.setName("Jack");
+  print(employee.getName());
 }
