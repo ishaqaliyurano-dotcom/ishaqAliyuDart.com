e823ef83471bcd00c32765c66ce52313041b5430
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 14:42:50 +0300

    change list value

--- collection.dart
+++ collection.dart
@@ -1,6 +1,6 @@
 void main() {
-  var list = [210, 21, 22, 33, 44, 55];
-
-  print(list.indexOf(22));
-  print(list.indexOf(33));
+  List<String> names = ["Raj", "John", "Rocky"];
+  names[1] = "Bill";
+  names[2] = "Elon";
+  print(names);
 }
