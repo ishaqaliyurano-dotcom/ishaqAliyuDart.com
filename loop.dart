ab2a085fb40a7b802d38e2c21fd2be04c91275f0
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 10:55:47 +0300

    even no. for loop

--- loop.dart
+++ loop.dart
@@ -1,10 +1,7 @@
 void main() {
-  int total = 0;
-  int n = 100; // change as per required
-
-  for (int i = 1; i <= n; i++) {
-    total = total + i;
+  for (int i = 50; i <= 100; i++) {
+    if (i % 2 == 0) {
+      print(i);
+    }
   }
-
-  print("Total is $total");
 }
