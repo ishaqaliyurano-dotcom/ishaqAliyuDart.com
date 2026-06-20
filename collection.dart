dcaccadc3a7e6bf630e7f3f5f0e5e87a01452856
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 15:32:05 +0300

    removing item from map

--- collection.dart
+++ collection.dart
@@ -1,11 +1,10 @@
 void main() {
-  Map<String, double> expenses = {'sun': 3000.0, 'mon': 3000.0, 'tue': 3234.0};
+  Map<String, String> countryCapital = {
+    'USA': 'Nothing',
+    'India': 'New Delhi',
+    'China': 'Beijing',
+  };
 
-  // For Keys
-  print("Does Map contain key sun: ${expenses.containsKey("sun")}");
-  print("Does Map contain key abc: ${expenses.containsKey("abc")}");
-
-  // For Values
-  print("Does Map contain value 3000.0: ${expenses.containsValue(3000.0)}");
-  print("Does Map contain value 100.0: ${expenses.containsValue(100.0)}");
+  countryCapital.remove("USA");
+  print(countryCapital);
 }
