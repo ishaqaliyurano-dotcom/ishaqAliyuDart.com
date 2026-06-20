e45584236f0d7fa8a04651433609c370ca74688e
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 17:30:34 +0300

    declaring class

---
+++ OOP.dart
@@ -0,0 +1,16 @@
+class Animal {
+  String? name;
+  int? numberOfLegs;
+  int? lifeSpan;
+
+  void display() {
+    print("Animal name: $name.");
+    print("Number of Legs: $numberOfLegs.");
+    print("Life Span: $lifeSpan.");
+  }
+}
+
+void main() {
+  Animal animal = Animal();
+  animal.display();
+}
