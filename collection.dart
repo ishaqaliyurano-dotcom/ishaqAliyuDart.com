cf9fa22ee03c2274d1ba252cbd6bc9685c46ce76
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 15:14:53 +0300

    check available value

--- collection.dart
+++ collection.dart
@@ -1,11 +1,5 @@
 void main() {
-  // declaring fruits as Set
-  Set<String> fruits = {"Apple", "Orange", "Mango", "Banana"};
-
-  // using different properties of Set
-  print("First Value is ${fruits.first}");
-  print("Last Value is ${fruits.last}");
-  print("Is fruits empty? ${fruits.isEmpty}");
-  print("Is fruits not empty? ${fruits.isNotEmpty}");
-  print("The length of fruits is ${fruits.length}");
+  Set<String> fruits = {"Apple", "Orange", "Mango"};
+  print(fruits.contains("Mango"));
+  print(fruits.contains("Lemon"));
 }
