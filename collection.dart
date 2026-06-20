e11f332b943edc4b9bd518b8fb444deb72c201d1
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 15:15:53 +0300

    add & remove item in set

--- collection.dart
+++ collection.dart
@@ -1,5 +1,11 @@
 void main() {
   Set<String> fruits = {"Apple", "Orange", "Mango"};
-  print(fruits.contains("Mango"));
-  print(fruits.contains("Lemon"));
+
+  fruits.add("Lemon");
+  fruits.add("Grape");
+
+  print("After Adding Lemon and Grape: $fruits");
+
+  fruits.remove("Apple");
+  print("After Removing Apple: $fruits");
 }
