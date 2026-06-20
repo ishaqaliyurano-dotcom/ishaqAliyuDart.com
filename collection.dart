b3dbf959e149ece513686b5ce20234d7f0c0499f
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 15:17:03 +0300

    add multi element

--- collection.dart
+++ collection.dart
@@ -1,11 +1,5 @@
 void main() {
-  Set<String> fruits = {"Apple", "Orange", "Mango"};
-
-  fruits.add("Lemon");
-  fruits.add("Grape");
-
-  print("After Adding Lemon and Grape: $fruits");
-
-  fruits.remove("Apple");
-  print("After Removing Apple: $fruits");
+  Set<int> numbers = {10, 20, 30};
+  numbers.addAll([40, 50]);
+  print("After adding 40 and 50: $numbers");
 }
