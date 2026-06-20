2c4760d88100896c845b6e0ed4342180a71f01e6
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 15:27:09 +0300

    map properties

--- collection.dart
+++ collection.dart
@@ -1,8 +1,9 @@
 void main() {
-  Map<String, String> countryCapital = {
-    'USA': 'Washington, D.C.',
-    'India': 'New Delhi',
-    'China': 'Beijing',
-  };
-  print(countryCapital["USA"]);
+  Map<String, double> expenses = {'sun': 3000.0, 'mon': 3000.0, 'tue': 3234.0};
+
+  print("All keys of Map: ${expenses.keys}");
+  print("All values of Map: ${expenses.values}");
+  print("Is Map empty: ${expenses.isEmpty}");
+  print("Is Map not empty: ${expenses.isNotEmpty}");
+  print("Length of map is: ${expenses.length}");
 }
