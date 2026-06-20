8c563fdab1a17b76ee29b19a7b0ab6b69c810b32
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 18:11:38 +0300

    declaring constructor

--- OOP.dart
+++ OOP.dart
@@ -1,20 +1,23 @@
-class Animal {
+class Student {
   String? name;
-  int? numberOfLegs;
-  int? lifeSpan;
+  int? age;
+  int? rollNumber;
 
-  void display() {
-    print("Animal name: $name.");
-    print("Number of Legs: $numberOfLegs.");
-    print("Life Span: $lifeSpan.");
+  // Constructor
+  Student(String name, int age, int rollNumber) {
+    print(
+      "Constructor called",
+    ); // this is for checking the constructor is called or not.
+    this.name = name;
+    this.age = age;
+    this.rollNumber = rollNumber;
   }
 }
 
 void main() {
-  // Here animal is object of class Animal.
-  Animal animal = Animal();
-  animal.name = "Lion";
-  animal.numberOfLegs = 4;
-  animal.lifeSpan = 10;
-  animal.display();
+  // Here student is object of class Student.
+  Student student = Student("John", 20, 1);
+  print("Name: ${student.name}");
+  print("Age: ${student.age}");
+  print("Roll Number: ${student.rollNumber}");
 }
