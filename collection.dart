8b097938f7f2a6ffeef50b451e827a2bdb47b0a4
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 15:21:35 +0300

    intersect in set

--- collection.dart
+++ collection.dart
@@ -2,7 +2,7 @@ void main() {
   Set<String> fruits1 = {"Apple", "Orange", "Mango"};
   Set<String> fruits2 = {"Apple", "Grapes", "Banana"};
 
-  final differenceSet = fruits1.difference(fruits2);
+  final intersectionSet = fruits1.intersection(fruits2);
 
-  print(differenceSet);
+  print(intersectionSet);
 }
