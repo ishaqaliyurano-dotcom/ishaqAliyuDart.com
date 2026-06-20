833bdfc9058c3c04cfb531bafaaaaf31f9cc2082
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 22:05:15 +0300

    enum in dart

--- OOP.dart
+++ OOP.dart
@@ -1,20 +1,26 @@
-class Animal {
-  void eat() {
-    print("Animal is eating");
-  }
-}
+enum Gender { Male, Female, Other }
 
-class Dog extends Animal {
-  @override
-  void eat() {
-    print("Dog is eating");
+class Person {
+  // Properties
+  String? firstName;
+  String? lastName;
+  Gender? gender;
+
+  // Constructor
+  Person(this.firstName, this.lastName, this.gender);
+
+  // display() method
+  void display() {
+    print("First Name: $firstName");
+    print("Last Name: $lastName");
+    print("Gender: $gender");
   }
 }
 
 void main() {
-  Animal animal = Animal();
-  animal.eat();
+  Person p1 = Person("John", "Doe", Gender.Male);
+  p1.display();
 
-  Dog dog = Dog();
-  dog.eat();
+  Person p2 = Person("Menuka", "Sharma", Gender.Female);
+  p2.display();
 }
