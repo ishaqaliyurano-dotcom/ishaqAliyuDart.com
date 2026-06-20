662e88da8f19c262f636f5482bbb17221d26b347
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 14:45:13 +0300

    isEmpty list

--- collection.dart
+++ collection.dart
@@ -1,5 +1,8 @@
 void main() {
   List<String> drinks = ["water", "juice", "milk", "coke"];
-  print("First element of the List is: ${drinks.first}");
-  print("Last element of the List is: ${drinks.last}");
+  List<int> ages = [];
+  print("Is drinks Empty: " + drinks.isEmpty.toString());
+  print("Is drinks not Empty: " + drinks.isNotEmpty.toString());
+  print("Is ages Empty: " + ages.isEmpty.toString());
+  print("Is ages not Empty: " + ages.isNotEmpty.toString());
 }
