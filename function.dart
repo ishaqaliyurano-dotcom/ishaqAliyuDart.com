0c8b931aa94c9e6f1518277f134e7e2d97886ee3
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 14:27:49 +0300

    anonymous function

--- function.dart
+++ function.dart
@@ -1,9 +1,7 @@
-void printInfo(String name, String gender, [String title = "sir/ma'am"]) {
-  print("Hello $title $name your gender is $gender.");
-}
-
 void main() {
-  printInfo("John", "Male");
-  printInfo("John", "Male", "Mr.");
-  printInfo("Kavya", "Female", "Ms.");
+  const fruits = ["Apple", "Mango", "Banana", "Orange"];
+
+  fruits.forEach((fruit) {
+    print(fruit);
+  });
 }
