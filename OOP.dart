7f176a53f3de8cb882f7cc59501a993ddcbbb597
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 18:03:34 +0300

    class & object

--- OOP.dart
+++ OOP.dart
@@ -1,25 +1,20 @@
-class Car {
+class Animal {
   String? name;
-  String? color;
-  int? numberOfSeats;
+  int? numberOfLegs;
+  int? lifeSpan;
 
-  void start() {
-    print("$name Car Started.");
+  void display() {
+    print("Animal name: $name.");
+    print("Number of Legs: $numberOfLegs.");
+    print("Life Span: $lifeSpan.");
   }
 }
 
 void main() {
-  // Here car is object of class Car.
-  Car car = Car();
-  car.name = "BMW";
-  car.color = "Red";
-  car.numberOfSeats = 4;
-  car.start();
-
-  // Here car2 is another object of class Car.
-  Car car2 = Car();
-  car2.name = "Audi";
-  car2.color = "Black";
-  car2.numberOfSeats = 4;
-  car2.start();
+  // Here animal is object of class Animal.
+  Animal animal = Animal();
+  animal.name = "Lion";
+  animal.numberOfLegs = 4;
+  animal.lifeSpan = 10;
+  animal.display();
 }
