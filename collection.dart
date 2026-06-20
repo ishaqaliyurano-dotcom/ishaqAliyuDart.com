7e03f6e332291fd9e12bbb99df5d63940439f764
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 14:56:18 +0300

    removing last element

--- collection.dart
+++ collection.dart
@@ -1,6 +1,6 @@
 void main() {
   var list = [10, 20, 30, 40, 50];
-  print("List before removing element : ${list}");
-  list.remove(30);
-  print("List after removing element : ${list}");
+  print("List before removing element:${list}");
+  list.removeLast();
+  print("List after removing last element:${list}");
 }
