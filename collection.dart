799619bcf7a1f1bad029f293092218f93829748b
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 15:27:56 +0300

    adding element to map

--- collection.dart
+++ collection.dart
@@ -1,9 +1,10 @@
 void main() {
-  Map<String, double> expenses = {'sun': 3000.0, 'mon': 3000.0, 'tue': 3234.0};
-
-  print("All keys of Map: ${expenses.keys}");
-  print("All values of Map: ${expenses.values}");
-  print("Is Map empty: ${expenses.isEmpty}");
-  print("Is Map not empty: ${expenses.isNotEmpty}");
-  print("Length of map is: ${expenses.length}");
+  Map<String, String> countryCapital = {
+    'USA': 'Washington, D.C.',
+    'India': 'New Delhi',
+    'China': 'Beijing',
+  };
+  // Adding New Item
+  countryCapital['Japan'] = 'Tokio';
+  print(countryCapital);
 }
