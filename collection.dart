81e7c13424252837ecf1a7c907a745f1279497c8
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 15:04:24 +0300

    set

--- collection.dart
+++ collection.dart
@@ -1,7 +1,4 @@
 void main() {
-  List<String> names = ["Raj", "John", "Rocky"];
-  List<String> names2 = ["Mike", "Subash", "Mark"];
-
-  List<String> allNames = [...names, ...names2];
-  print(allNames);
+  Set<String> fruits = {"Apple", "Orange", "Mango"};
+  print(fruits);
 }
