af539fe80d60a4c1f652c685ec13ba3b707024e2
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 14:50:21 +0300

    remove item

--- collection.dart
+++ collection.dart
@@ -1,6 +1,6 @@
 void main() {
-  var list = [10, 15, 20, 25, 30];
-  print("List before updation: ${list}");
-  list.replaceRange(0, 4, [5, 6, 7, 8]);
-  print("List after updation using replaceAll() function : ${list}");
+  var list = [10, 20, 30, 40, 50];
+  print("List before removing element : ${list}");
+  list.remove(30);
+  print("List after removing element : ${list}");
 }
