94a38cae0e0ccbaf5d077e2b4aa4bf16cb8ffbed
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 15:11:34 +0300

    set properties

--- collection.dart
+++ collection.dart
@@ -1,4 +1,11 @@
 void main() {
-  Set<String> fruits = {"Apple", "Orange", "Mango"};
-  print(fruits);
+  // declaring fruits as Set
+  Set<String> fruits = {"Apple", "Orange", "Mango", "Banana"};
+
+  // using different properties of Set
+  print("First Value is ${fruits.first}");
+  print("Last Value is ${fruits.last}");
+  print("Is fruits empty? ${fruits.isEmpty}");
+  print("Is fruits not empty? ${fruits.isNotEmpty}");
+  print("The length of fruits is ${fruits.length}");
 }
