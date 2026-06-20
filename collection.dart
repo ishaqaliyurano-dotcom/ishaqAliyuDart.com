0ed1419b8aff1663572d28d76bdbea75975aec43
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 14:41:08 +0300

    index list

--- collection.dart
+++ collection.dart
@@ -1,4 +1,10 @@
 void main() {
-  var list1 = [210, 21, 22, 33, 44, 55];
-  print(list1);
+  var list = [210, 21, 22, 33, 44, 55];
+
+  print(list[0]);
+  print(list[1]);
+  print(list[2]);
+  print(list[3]);
+  print(list[4]);
+  print(list[5]);
 }
