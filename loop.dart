e053ad334976dde359d4a354df308a084da47886
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 11:16:03 +0300

    while loop

--- loop.dart
+++ loop.dart
@@ -1,13 +1,7 @@
 void main() {
-  List<int> numbers = [1, 2, 3, 4, 5];
-
-  int total = 0;
-
-  numbers.forEach((num) => total = total + num);
-
-  print("Total is $total.");
-
-  double avg = total / (numbers.length);
-
-  print("Average is $avg.");
+  int i = 1;
+  while (i <= 10) {
+    print(i);
+    i++;
+  }
 }
