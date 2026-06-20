6b466e40b1633bc3fd19e1eda69c65a5a7c272e5
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 10:40:54 +0300

    switch-case

--- loop.dart
+++ loop.dart
@@ -1,20 +1,24 @@
 void main() {
-  int noOfMonth = 5;
+  var dayOfWeek = 5;
+  switch (dayOfWeek) {
+    case 1:
+      print("Day is Sunday.");
+      break;
+    case 2:
+      print("Day is Monday.");
+      break;
+    case 3:
+      print("Day is Tuesday.");
+      break;
+    case 4:
+      print("Day is Wednesday.");
+      break;
+    case 5:
+      print("Day is Thursday.");
+      break;
 
-  // Check the no of month
-  if (noOfMonth == 1) {
-    print("The month is jan");
-  } else if (noOfMonth == 2) {
-    print("The month is feb");
-  } else if (noOfMonth == 3) {
-    print("The month is march");
-  } else if (noOfMonth == 4) {
-    print("The month is april");
-  } else if (noOfMonth == 5) {
-    print("The month is may");
-  } else if (noOfMonth == 6) {
-    print("The month is june");
-  } else {
-    print("Invalid option given.");
+    default:
+      print("Invalid Weekday.");
+      break;
   }
 }
