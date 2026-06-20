92c562854221b0bdba13411b2b414ffddb838560
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 11:09:56 +0300

    for each and avg

--- loop.dart
+++ loop.dart
@@ -1,4 +1,13 @@
 void main() {
-  List<String> footballplayers = ['Ronaldo', 'Messi', 'Neymar', 'Hazard'];
-  footballplayers.forEach((names) => print(names));
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  int total = 0;
+
+  numbers.forEach((num) => total = total + num);
+
+  print("Total is $total.");
+
+  double avg = total / (numbers.length);
+
+  print("Average is $avg.");
 }
