0302daf7211b667bbebfb7dd4a8b250b0782a51a
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 17:07:09 +0300

    write file

--- fileHandling.dart
+++ fileHandling.dart
@@ -1,14 +1,10 @@
+// dart program to write to file
 import 'dart:io';
 
 void main() {
   // open file
   File file = File('test.txt');
-  // get file location
-  print('File path: ${file.path}');
-  // get absolute path
-  print('File absolute path: ${file.absolute.path}');
-  // get file size
-  print('File size: ${file.lengthSync()} bytes');
-  // get last modified time
-  print('Last modified: ${file.lastModifiedSync()}');
+  // write to file
+  file.writeAsStringSync('Welcome to test.txt file.');
+  print('File written.');
 }
