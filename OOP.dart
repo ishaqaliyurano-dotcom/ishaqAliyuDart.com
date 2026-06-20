ac060710a18e52978600b578d173b83b88dc1c47
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 22:08:41 +0300

    interface in dart

--- OOP.dart
+++ OOP.dart
@@ -1,37 +1,32 @@
-abstract class Shape {
-  int dim1, dim2;
-  // Constructor
-  Shape(this.dim1, this.dim2);
-  // Abstract method
-  void area();
-}
+// creating an interface using concrete class
+class Laptop {
+  // method
+  turnOn() {
+    print('Laptop turned on');
+  }
 
-class Rectangle extends Shape {
-  // Constructor
-  Rectangle(int dim1, int dim2) : super(dim1, dim2);
-
-  // Implementation of area()
-  @override
-  void area() {
-    print('The area of the rectangle is ${dim1 * dim2}');
+  // method
+  turnOff() {
+    print('Laptop turned off');
   }
 }
 
-class Triangle extends Shape {
-  // Constructor
-  Triangle(int dim1, int dim2) : super(dim1, dim2);
-
-  // Implementation of area()
+class MacBook implements Laptop {
+  // implementation of turnOn()
   @override
-  void area() {
-    print('The area of the triangle is ${0.5 * dim1 * dim2}');
+  turnOn() {
+    print('MacBook turned on');
+  }
+
+  // implementation of turnOff()
+  @override
+  turnOff() {
+    print('MacBook turned off');
   }
 }
 
 void main() {
-  Rectangle rectangle = Rectangle(10, 20);
-  rectangle.area();
-
-  Triangle triangle = Triangle(10, 20);
-  triangle.area();
+  var macBook = MacBook();
+  macBook.turnOn();
+  macBook.turnOff();
 }
