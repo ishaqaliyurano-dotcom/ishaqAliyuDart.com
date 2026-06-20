d9fe6ede55a40962b0fd8ac61eb2ea95ca5784e2
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 10:48:30 +0300

    ternary

--- loop.dart
+++ loop.dart
@@ -1,19 +1,6 @@
 void main() {
-  const weather = "cloudy";
-
-  switch (weather) {
-    case "sunny":
-      print("Its a sunny day. Put sunscreen.");
-      break;
-    case "snowy":
-      print("Get your skis.");
-      break;
-    case "cloudy":
-    case "rainy":
-      print("Please bring umbrella.");
-      break;
-    default:
-      print("Sorry I am not familiar with such weather.");
-      break;
-  }
+  int num1 = 10;
+  int num2 = 15;
+  int max = (num1 > num2) ? num1 : num2;
+  print("The greatest number is $max");
 }
