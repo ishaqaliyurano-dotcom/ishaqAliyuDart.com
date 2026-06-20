20bdbb6d20298230fc101811fff8b7c46af4373e
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 22:01:17 +0300

    accessing super

--- OOP.dart
+++ OOP.dart
@@ -1,19 +1,17 @@
-class Laptop {
-  // Method
-  void show() {
-    print("Laptop show method");
-  }
+class Car {
+  int noOfSeats = 4;
 }
 
-class MacBook extends Laptop {
-  void show() {
-    super.show(); // Calling the show method of the parent class
-    print("MacBook show method");
+class Tesla extends Car {
+  int noOfSeats = 6;
+
+  void display() {
+    print("No of seats in Tesla: $noOfSeats");
+    print("No of seats in Car: ${super.noOfSeats}");
   }
 }
 
 void main() {
-  // Creating an object of the MacBook class
-  MacBook macbook = MacBook();
-  macbook.show();
+  var tesla = Tesla();
+  tesla.display();
 }
