e9d1448f51ee5b14f8e6bd6391cb17f690005a9a
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 10:29:05 +0300

    else-if

--- loop.dart
+++ loop.dart
@@ -1,8 +1,20 @@
 void main() {
-  bool isMarried = false;
-  if (isMarried) {
-    print("You are married.");
+  int noOfMonth = 5;
+
+  // Check the no of month
+  if (noOfMonth == 1) {
+    print("The month is jan");
+  } else if (noOfMonth == 2) {
+    print("The month is feb");
+  } else if (noOfMonth == 3) {
+    print("The month is march");
+  } else if (noOfMonth == 4) {
+    print("The month is april");
+  } else if (noOfMonth == 5) {
+    print("The month is may");
+  } else if (noOfMonth == 6) {
+    print("The month is june");
   } else {
-    print("You are single.");
+    print("Invalid option given.");
   }
 }
