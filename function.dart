645e5ac76ec34553cc021ba7f0ba6256ee4f9f0b
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 14:34:10 +0300

    scope in dart

--- function.dart
+++ function.dart
@@ -1,12 +1,4 @@
-// arrow function that calculate interest
-double calculateInterest(double principal, double rate, double time) =>
-    principal * rate * time / 100;
-
 void main() {
-  double principal = 5000;
-  double time = 3;
-  double rate = 3;
-
-  double result = calculateInterest(principal, rate, time);
-  print("The simple interest is $result.");
+  String text = "I am text inside main. Anyone can't access me.";
+  print(text);
 }
