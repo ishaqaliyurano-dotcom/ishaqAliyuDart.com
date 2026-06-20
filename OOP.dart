08affb0f1b653a773fc83dc641df5ed4fef64d6e
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 22:13:09 +0300

    generic method

--- OOP.dart
+++ OOP.dart
@@ -1,15 +1,11 @@
-// Using Generics
-class Data<T> {
-  T data;
-  Data(this.data);
+// Define generic method
+T genericMethod<T>(T value) {
+  return value;
 }
 
 void main() {
-  // create an object of type int and double
-  Data<int> intData = Data<int>(10);
-  Data<double> doubleData = Data<double>(10.5);
-
-  // print the data
-  print("IntData: ${intData.data}");
-  print("DoubleData: ${doubleData.data}");
+  // call the generic method
+  print("Int: ${genericMethod<int>(10)}");
+  print("Double: ${genericMethod<double>(10.5)}");
+  print("String: ${genericMethod<String>("Hello")}");
 }
