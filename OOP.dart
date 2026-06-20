39028b1484476ccdbcc8217ebcb6c8331f3a6a4a
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 21:54:00 +0300

    single inheritance

--- OOP.dart
+++ OOP.dart
@@ -1,36 +1,23 @@
-class Person {
+class Car {
   // Properties
   String? name;
-  int? age;
-
-  // Method
-  void display() {
-    print("Name: $name");
-    print("Age: $age");
-  }
+  double? price;
 }
 
-// Here In student class, we are extending the
-// properties and methods of the Person class
-class Student extends Person {
-  // Fields
-  String? schoolName;
-  String? schoolAddress;
-
-  // Method
-  void displaySchoolInfo() {
-    print("School Name: $schoolName");
-    print("School Address: $schoolAddress");
+class Tesla extends Car {
+  // Method to display the values of the properties
+  void display() {
+    print("Name: ${name}");
+    print("Price: ${price}");
   }
 }
 
 void main() {
-  // Creating an object of the Student class
-  var student = Student();
-  student.name = "John";
-  student.age = 20;
-  student.schoolName = "ABC School";
-  student.schoolAddress = "New York";
-  student.display();
-  student.displaySchoolInfo();
+  // Create an object of Tesla class
+  Tesla t = new Tesla();
+  // setting values to the object
+  t.name = "Tesla Model 3";
+  t.price = 50000.00;
+  // Display the values of the object
+  t.display();
 }
