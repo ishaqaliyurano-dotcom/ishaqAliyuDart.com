21c6f2dc0dc5f160060759d3a8b3e333f7314e65
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 15:31:31 +0300

    check map contain keys or values

--- collection.dart
+++ collection.dart
@@ -1,11 +1,11 @@
 void main() {
   Map<String, double> expenses = {'sun': 3000.0, 'mon': 3000.0, 'tue': 3234.0};
 
-  // Without List
-  print("All keys of Map: ${expenses.keys}");
-  print("All values of Map: ${expenses.values}");
+  // For Keys
+  print("Does Map contain key sun: ${expenses.containsKey("sun")}");
+  print("Does Map contain key abc: ${expenses.containsKey("abc")}");
 
-  // With List
-  print("All keys of Map with List: ${expenses.keys.toList()}");
-  print("All values of Map with List: ${expenses.values.toList()}");
+  // For Values
+  print("Does Map contain value 3000.0: ${expenses.containsValue(3000.0)}");
+  print("Does Map contain value 100.0: ${expenses.containsValue(100.0)}");
 }
