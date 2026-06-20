e3fabadebe8d2e9edc41ecedaa2a22b4ee959c0e
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-20 18:21:05 +0300

    parameter constructor with default value

--- OOP.dart
+++ OOP.dart
@@ -1,20 +1,17 @@
 class Student {
   String? name;
   int? age;
-  int? rollNumber;
 
   // Constructor
-  Student({String? name, int? age, int? rollNumber}) {
+  Student({String? name = "John", int? age = 0}) {
     this.name = name;
     this.age = age;
-    this.rollNumber = rollNumber;
   }
 }
 
 void main() {
   // Here student is object of class Student.
-  Student student = Student(name: "John", age: 20, rollNumber: 1);
+  Student student = Student();
   print("Name: ${student.name}");
   print("Age: ${student.age}");
-  print("Roll Number: ${student.rollNumber}");
 }
