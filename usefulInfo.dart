74f092fdbb203d0d8bd9fc708b32232eb996a47c
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-21 12:31:34 +0300

    add date in dart

--- usefulInfo.dart
+++ usefulInfo.dart
@@ -1,6 +1,6 @@
 void main() {
   DateTime myBirthday = DateTime.parse("1997-05-14");
-  myBirthday = myBirthday.subtract(Duration(days: 1));
+  myBirthday = myBirthday.add(Duration(days: 1));
   print("Year is " + myBirthday.year.toString());
   print("Month is " + myBirthday.month.toString());
   print("Day is " + myBirthday.day.toString());
