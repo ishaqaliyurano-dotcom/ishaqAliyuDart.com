c387660ea32205dbba1e2a7d0fa9b89629a32e24
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-21 12:32:31 +0300

    diff between date in dart

--- usefulInfo.dart
+++ usefulInfo.dart
@@ -1,7 +1,11 @@
 void main() {
   DateTime myBirthday = DateTime.parse("1997-05-14");
-  myBirthday = myBirthday.add(Duration(days: 1));
-  print("Year is " + myBirthday.year.toString());
-  print("Month is " + myBirthday.month.toString());
-  print("Day is " + myBirthday.day.toString());
+  DateTime today = DateTime.now();
+  Duration diff = today.difference(myBirthday);
+  print("Difference in days: " + diff.inDays.toString());
+  print("Difference in hours: " + diff.inHours.toString());
+  print("Difference in minutes: " + diff.inMinutes.toString());
+  print("Difference in seconds: " + diff.inSeconds.toString());
+  print("Difference in milliseconds: " + diff.inMilliseconds.toString());
+  print("Difference in microseconds: " + diff.inMicroseconds.toString());
 }
