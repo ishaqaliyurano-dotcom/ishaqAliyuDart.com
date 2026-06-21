62c62cc8581f5f09b36d9f58f79b2d66ea05f949
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-21 09:22:35 +0300

    asynchronous function

--- asynchronous.dart
+++ asynchronous.dart
@@ -1,11 +1,14 @@
-// function that returns a future
-Future<String> getUserName() async {
-  return Future.delayed(Duration(seconds: 2), () => 'Mark');
-}
-
-// main function
 void main() {
   print("Start");
-  getUserName().then((value) => print(value));
+  getData();
   print("End");
+}
+
+void getData() async {
+  String data = await middleFunction();
+  print(data);
+}
+
+Future<String> middleFunction() {
+  return Future.delayed(Duration(seconds: 5), () => "Hello");
 }
