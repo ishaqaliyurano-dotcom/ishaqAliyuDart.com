48f9f5fdc17fd736bc94d329fee138a54602a811
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-21 09:06:10 +0300

    type promotion in dart

--- nullSafety.dart
+++ nullSafety.dart
@@ -1,8 +1,9 @@
-class Person {
-  String? name;
-  Person(this.name);
-}
-
 void main() {
-  Person person = Person(null); // Works
+  Object name = "Pratik";
+  // print(name.length) will not work because Dart doesn't know that name is a String
+
+  if (name is String) {
+    // name promoted from Object to String
+    print("The length of name is ${name.length}");
+  }
 }
