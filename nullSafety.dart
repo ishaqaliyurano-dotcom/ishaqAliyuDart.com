93210bb4957424eb214998d86d9ac74fb77bdf14
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-21 09:29:13 +0300

    nullable items

--- nullSafety.dart
+++ nullSafety.dart
@@ -1,10 +1,5 @@
-// Try to use null assertion operator(!) to print the length of the String or return null if the variable is null
-int findLength(String? name) {
-  // add null assertion operator here
-  return name.length;
-}
-
 void main() {
-  int? length = findLength("Hello");
-  print("The length of the string is $length");
+  // list of nullable ints
+  List<int?> items = [1, 2, null, 4];
+  print(items);
 }
