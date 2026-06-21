28fad0142c6f216b288acbf90ca168b32d82c081
ishaqRano <ishaqaliyurano@gmail.com>
2026-06-21 12:35:36 +0300

    extension in dart

--- usefulInfo.dart
+++ usefulInfo.dart
@@ -1,11 +1,10 @@
 void main() {
-  DateTime myBirthday = DateTime.parse("1997-05-14");
-  DateTime today = DateTime.now();
-  Duration diff = today.difference(myBirthday);
-  print("Difference in days: " + diff.inDays.toString());
-  print("Difference in hours: " + diff.inHours.toString());
-  print("Difference in minutes: " + diff.inMinutes.toString());
-  print("Difference in seconds: " + diff.inSeconds.toString());
-  print("Difference in milliseconds: " + diff.inMilliseconds.toString());
-  print("Difference in microseconds: " + diff.inMicroseconds.toString());
+  String name = "john";
+  print(name.capitalize());
+}
+
+extension StringExtension on String {
+  String capitalize() {
+    return "${this[0].toUpperCase()}${this.substring(1)}";
+  }
 }
